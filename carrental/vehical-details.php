74f475e7f1a9112907d19b09745c67694e59108b
parthrokde03 <rokdeparth26@gmail.com>
2024-09-05 20:07:22 +0530

    Changing dollar symbol to rupee.

--- carrental/vehical-details.php
+++ carrental/vehical-details.php
@@ -396,7 +396,7 @@ foreach($results as $result)
             </div>
             <div class="product-listing-content">
               <h5><a href="vehical-details.php?vhid=<?php echo htmlentities($result->id);?>"><?php echo htmlentities($result->BrandName);?> , <?php echo htmlentities($result->VehiclesTitle);?></a></h5>
-              <p class="list-price"><?php echo htmlentities($result->PricePerDay);?></p>
+              <p class="list-price">₹<?php echo htmlentities($result->PricePerDay);?></p>
           
               <ul class="features_list">
                 
